85453269209051ee109c2bd59ea5f646fa8a1872
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-12-21 00:21:36 -0500

    test: add unit tests for loss_run

--- tests/testthat/test-actuary-loss_run.R
+++ tests/testthat/test-actuary-loss_run.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("loss_run conversion works", {
+  trans <- claims_transactional
+  test_df <- loss_run(val_date = Sys.Date(), claims_transactional)
+  expect_s3_class(test_df, "tbl_df")
+  expect_s3_class(test_df, "data.frame")
+  expect_true(ncol(test_df) == 22)
+  expect_true(nrow(test_df) == length(unique(trans$claim_num)))
 })
