daf62bc46f4df1ad5823450850c0448c768f070b
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-01-04 21:17:13 -0500

    cleanup unused functions

--- R/utils-data.R
+++ R/utils-data.R
@@ -27,163 +27,6 @@ pull_unique <- function(df, var) {
     sort()
 }
 
-#' Get Common Columns
-#'
-#' Get common columns across two tables
-#'
-#' @param tables a list with two elements: x and y
-#' @keywords internal
-#' @return a list with two elements: x and y
-#' @importFrom crayon yellow bold
-#' @export
-#' @rdname data-utils
-get_common_columns <- function(tables) {
-  x <- tables$x
-  y <- tables$y
-
-  not.in.x <- colnames(y)[!c(colnames(y) %in% colnames(x))]
-  not.in.y <- colnames(x)[!c(colnames(x) %in% colnames(y))]
-
-  if (length(not.in.x) > 0) {
-    cat(crayon::yellow(paste0(
-      "Column(s) '", paste(not.in.x, collapse = "', '"),
-      "' are not in ", crayon::bold("LHS"),
-      " so they have been removed from the comparison.\n"
-    )))
-  }
-  if (length(not.in.y) > 0) {
-    cat(crayon::yellow(paste0(
-      "Column(s) '", paste(not.in.y, collapse = "', '"),
-      "' are not in ", crayon::bold("RHS"),
-      " so they have been removed from the comparison.\n"
-    )))
-  }
-
-  common.columns <- colnames(x)[colnames(x) %in% colnames(y)]
-  if (length(common.columns) == 0) {
-    stop("Tables don't have common columns.", call. = FALSE)
-  }
-
-  x <- x[common.columns]
-  y <- y[common.columns]
-
-  tables <- list(x = x, y = y)
-  return(tables)
-}
-
-#' Get Common Number of Rows
-#'
-#' Get common number of rows across two tables
-#'
-#' @param tables a list with two elements: x and y
-#' @keywords internal
-#' @return a list with two elements: x and y
-#' @importFrom crayon yellow bold
-#' @export
-#' @rdname data-utils
-get_common_nr_rows <- function(tables) {
-  x <- tables$x
-  y <- tables$y
-
-  n1 <- nrow(x)
-  n2 <- nrow(y)
-  if (n1 > n2) {
-    cat(crayon::yellow(paste0(
-      crayon::bold("LHS"), " has more rows than ",
-      crayon::bold("RHS"), ". The last ", n1 - n2,
-      " row(s) of ", crayon::bold("LHS"),
-      " have been removed.\n"
-    )))
-    x <- as.data.frame(x[1:n2, ])
-  }
-
-  if (n2 > n1) {
-    cat(crayon::yellow(paste0(
-      crayon::bold("RHS"), " has more rows than ",
-      crayon::bold("LHS"), ". The last ", n2 - n1,
-      " row(s) of ", crayon::bold("RHS"),
-      " have been removed.\n"
-    )))
-    y <- as.data.frame(y[1:n1, ])
-  }
-
-  tables <- list(x = x, y = y)
-  return(tables)
-}
-
-#' Get Common Types
-#'
-#' Get common types of columns across two tables
-#'
-#' @param tables a list with two elements: x and y
-#' @keywords internal
-#' @return a list with two elements: x and y
-#' @importFrom crayon yellow bold
-#' @importFrom dplyr left_join rename mutate
-#' @importFrom purrr pmap_chr
-#' @export
-#' @rdname data-utils
-get_common_types <- function(tables) {
-  x <- tables$x
-  y <- tables$y
-
-  util.types <- data.frame(
-    ColType = c("logical", "integer", "double", "character"),
-    Power = 1:4,
-    stringsAsFactors = FALSE
-  )
-
-  col.types.x <- c()
-  col.types.y <- c()
-  for (i in 1:ncol(x)) col.types.x[i] <- typeof(x[, i])
-  for (i in 1:ncol(y)) col.types.y[i] <- typeof(y[, i])
-
-  col.types <- data.frame(
-    Column = colnames(x),
-    ColTypeX = col.types.x,
-    ColTypeY = col.types.y,
-    stringsAsFactors = FALSE
-  )
-
-  types.tally <- col.types %>%
-    dplyr::left_join(util.types, by = c("ColTypeX" = "ColType")) %>%
-    dplyr::rename(PowerX = Power) %>%
-    dplyr::left_join(util.types, by = c("ColTypeY" = "ColType")) %>%
-    dplyr::rename(PowerY = Power) %>%
-    dplyr::mutate(Same = (ColTypeX == ColTypeY)) %>%
-    dplyr::mutate(
-      StrongerType =
-        purrr::pmap_chr(
-          list(PowerX, PowerY, ColTypeX, ColTypeY),
-          function(p1, p2, ct1, ct2) if (p1 > p2) ct1 else ct2
-        )
-    )
-
-  for (r in 1:nrow(types.tally)) {
-    if (types.tally[r, "Same"] == FALSE) {
-      row <- types.tally[r, ]
-      if (row$PowerY > row$PowerX) {
-        cat(crayon::yellow(paste0(
-          "Column '", row$Column, "' in ", crayon::bold("LHS"),
-          " has been coerced from ", row$ColTypeX,
-          " to ", row$StrongerType, ".\n"
-        )))
-        x[, row$Column] <- as(x[, row$Column], type = row$StrongerType)
-      } else {
-        cat(crayon::yellow(paste0(
-          "Column '", row$Column, "' in ", crayon::bold("RHS"),
-          " has been coerced from ", row$ColTypeY,
-          " to ", row$StrongerType, ".\n"
-        )))
-        y[, row$column] <- as(y[, row$Column], type = row$StrongerType)
-      }
-    }
-  }
-
-  tables <- list(x = x, y = y)
-  return(tables)
-}
-
 #' Coalesce Join
 #'
 #' @param x x
@@ -195,6 +38,8 @@ get_common_types <- function(tables) {
 #'
 #' @return a [tibble][tibble::tibble-package]
 #' @export
+#' @importFrom dplyr union coalesce bind_cols
+#' @importFrom purrr map_dfc
 #' @importFrom dplyr union coalesce bind_cols
 #' @importFrom purrr map_dfc
 coalesce_join <- function(x,
@@ -252,6 +97,7 @@ coalesce_join <- function(x,
 #' data(losses)
 #' string <- doc_data(losses, "Loss Data", "Claims Data", FALSE)
 #' cat(string)
+#' @importFrom usethis use_r
 doc_data <- function(obj,
                      title = deparse(substitute(obj)),
                      description = "DATASET_DESCRIPTION",
