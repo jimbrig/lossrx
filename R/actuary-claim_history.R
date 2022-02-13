9579370334803b23f96f1d3a9781820f3cb3383e
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-02-12 20:34:40 -0500

    adjust functions

--- R/actuary-claim_history.R
+++ R/actuary-claim_history.R
@@ -14,7 +14,9 @@
 #' @importFrom lubridate as_date
 #' @importFrom shiny HTML
 #' @importFrom timevis timevis
-view_claim_history <- function(claim_id, claims_data = claims_transactional) {
+view_claim_history <- function(claim_id, claims_data = NULL) {
+
+  if (is.null(claim_data)) claims_data <- claims_transactional
 
   claim_dat <- dplyr::filter(claims_data, .data$claim_id == .env$claim_id) |>
     dplyr::arrange(.data$transaction_date)
@@ -27,7 +29,7 @@ view_claim_history <- function(claim_id, claims_data = claims_transactional) {
                                  `Report Date` = .data$report_date) |>
     dplyr::distinct()
 
-  close_date <- dplyr::filter(claim_dat, status == "Closed") |>
+  close_date <- dplyr::filter(claim_dat, .data$status == "Closed") |>
     dplyr::pull(.data$transaction_date) |>
     lubridate::as_date()
 
@@ -42,26 +44,26 @@ view_claim_history <- function(claim_id, claims_data = claims_transactional) {
     incr_case = .data$case - dplyr::lag(.data$case, default = 0, order_by = .data$transaction_date),
     incr_status = paste0(dplyr::lag(.data$status, default = "NEW", order_by = .data$transaction_date), " -> ", .data$status),
     status_details = case_when(
-      incr_status == "NEW -> Open" ~ "Status: Claim Opened",
-      incr_status == "Open -> Open" ~ "Status: No Change",
-      incr_status == "Closed -> Closed" ~ "Status: No Change",
-      incr_status == "Closed -> Open" ~ "Status: Claim Re-Opened",
-      incr_status == "Open -> Closed" ~ "Status: Claim Closed"
+      .data$incr_status == "NEW -> Open" ~ "Status: Claim Opened",
+      .data$incr_status == "Open -> Open" ~ "Status: No Change",
+      .data$incr_status == "Closed -> Closed" ~ "Status: No Change",
+      .data$incr_status == "Closed -> Open" ~ "Status: Claim Re-Opened",
+      .data$incr_status == "Open -> Closed" ~ "Status: Claim Closed"
     ),
     rept_details = case_when(
-      incr_rept == 0 ~ "Reported: No Change",
-      incr_rept > 0 ~ paste0("Reported: Increased by ", as.character(formattable::currency(.data$incr_rept))),
-      incr_rept < 0 ~ paste0("Reported: Decreased by ", as.character(formattable::currency(.data$incr_rept)))
+      .data$incr_rept == 0 ~ "Reported: No Change",
+      .data$incr_rept > 0 ~ paste0("Reported: Increased by ", as.character(formattable::currency(.data$incr_rept))),
+      .data$incr_rept < 0 ~ paste0("Reported: Decreased by ", as.character(formattable::currency(.data$incr_rept)))
     ),
     paid_details = case_when(
-      payment == 0 ~ "Paid: No Change",
-      payment > 0 ~ paste0("Paid: Increased by ", as.character(formattable::currency(.data$payment))),
-      payment < 0 ~ paste0("Paid: Decreased by ", as.character(formattable::currency(.data$payment)))
+      .data$payment == 0 ~ "Paid: No Change",
+      .data$payment > 0 ~ paste0("Paid: Increased by ", as.character(formattable::currency(.data$payment))),
+      .data$payment < 0 ~ paste0("Paid: Decreased by ", as.character(formattable::currency(.data$payment)))
     ),
     case_details = case_when(
-      incr_case == 0 ~ "Case Reserve: No Change",
-      incr_case > 0 ~ paste0("Case Reserve: Increased by ", as.character(formattable::currency(.data$incr_case))),
-      incr_case < 0 ~ paste0("Case Reserve: Decreased by ", as.character(formattable::currency(.data$incr_case)))
+      .data$incr_case == 0 ~ "Case Reserve: No Change",
+      .data$incr_case > 0 ~ paste0("Case Reserve: Increased by ", as.character(formattable::currency(.data$incr_case))),
+      .data$incr_case < 0 ~ paste0("Case Reserve: Decreased by ", as.character(formattable::currency(.data$incr_case)))
     )
   ) |>
     dplyr::rowwise() |>
