5b4610acc067bf459ef4f83bb1dbc84f99e27ffd
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-09-11 19:32:05 -0400

    config: Update dependencies.R

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -1,8 +1,9 @@
 # Remotes ----
 install.packages("remotes")
 remotes::install_github('Displayr/flipTime')
+remotes::install_github('rstudio/connections')
 # Attachments ----
-to_install <- c("cli", "crayon", "dbx", "dplyr", "fs", "glue", "here", "lubridate", "magrittr", "purrr", "readr", "rlang", "stringr", "tibble", "usethis")
+to_install <- c("cli", "config", "crayon", "dbx", "dplyr", "formattable", "fs", "glue", "here", "lubridate", "magrittr", "pool", "purrr", "randomNames", "readr", "rlang", "RPostgres", "shiny", "stringr", "tibble", "timevis", "usethis")
   for (i in to_install) {
     message(paste("looking for ", i))
     if (!requireNamespace(i)) {
